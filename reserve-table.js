97469383488d1f69c1da08e47359cc8d5a24c801
RY <91940087+RcapybaraY@users.noreply.github.com>
2023-10-21 20:35:21 +0800

    Add files via upload

---
+++ reserve-table.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+    let bookingStatus = [];
+    let selectTableId = null;
+    localStorage.setItem("bookedTable", JSON.stringify(bookingStatus));
+
+    $(".table").each(function () {
+        if (bookingStatus.toString().includes($(this).attr("id"))) {
+            $(this).addClass("booked");
+        }
+    });
+
+    $(".table").on("click", function () {
+        if ($(this).attr("class").includes("booked")) {
+            window.alert("This table is aready booked.")
+            $("#selected-table").html("Click a Table to book.");
+        } else {
+            selectTableId = $(this).attr("id");
+            $("#selected-table").html("Do you want to book Table " + selectTableId);
+        }
+    })
+
+    if (localStorage.getItem("bookedTable") != "") {
+        bookingStatus = JSON.parse(localStorage.getItem("bookedTable"));
+    }
+
+    $("#confirm").on("click", function () {
+        bookingStatus.push(selectTableId)
+        localStorage.setItem("bookedTable", JSON.stringify(bookingStatus));
+        $(".table").each(function () {
+            if (bookingStatus.toString().includes($(this).attr("id"))) {
+                $(this).addClass("booked");
+            }
+        });
+        $("#selected-table").html("You have selected Table" + selectTableId);
+        $("#confirm").addClass("d-none");
+    })
+
+    $("#dismiss").on("click", function () {
+        $("#selected-table").html("Click a Table to book.");
+        $("#confirm").removeClass("d-none");
+        bookingStatus.pop(selectTableId)
+        localStorage.setItem("bookedTable", JSON.stringify(bookingStatus));
+        $(".table").each(function () {
+            if (!(bookingStatus.toString().includes($(this).attr("id")))) {
+                $(this).removeClass("booked");
+            }
+        });
+    });
+
+});
